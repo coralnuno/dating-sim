2705c0839062d77c2c929211065476369e5f3907
coral <coralnuno@outlook.com>
2023-02-27 12:57:54 -0800

    c

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
-print('Hello World')
+#BASIC SET UP FOR GAME 
+
+#Importing and initiallizing pygame 
+import pygame, sys 
+
+class Game: 
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((1280,720))
+        self.clock = pygame.time.Clock()
+
+
+    def run(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+
+
+            dt = self.clock.tick() / 1000
+            pygame.display.update()
+
+if __name__ == '__main__':
+    game = Game()
+    game.run
+    
